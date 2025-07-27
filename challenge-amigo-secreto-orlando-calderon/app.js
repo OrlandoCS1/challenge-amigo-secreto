46e1b5e0972c67a3b2fcfb0c6c9ab22b4b16c94d
Orland_CS <139184502+OrlandoCS1@users.noreply.github.com>
2025-07-27 01:09:45 -0600

    Modificar archivo app.js con mi código
    Agregue mi código creado desde visual studio code en este archivo llamado app.js

--- challenge-amigo-secreto-orlando-calderon/app.js
+++ challenge-amigo-secreto-orlando-calderon/app.js
@@ -1,1 +1,48 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listaDeAmigos = []; // Esta es nuestra lista donde vamos a guardar todos nuestros amigos.
+
+let miLista = document.querySelector('#listaAmigos');
+let resultado = document.getElementById('resultado');
+
+function generarNumeroGanador(){
+    let numero = Math.floor(Math.random() * listaDeAmigos.length);
+    return numero;
+}
+
+
+function agregarEnLista(amigo) {
+            let elementoDelista = document.createElement('li');
+            elementoDelista.innerHTML = amigo;
+            miLista.appendChild(elementoDelista);
+   
+}
+
+function limpiarTextBox(){
+    document.querySelector('#amigo').value = '';
+}
+
+function sortearAmigo(){
+    let ganador = listaDeAmigos[generarNumeroGanador()];
+    console.log(ganador);
+    miLista.remove();
+    let enunciadoResultado = document.createElement('li');
+    enunciadoResultado.innerHTML = `el ganador es: ${ganador}`;
+    resultado.appendChild(enunciadoResultado);
+
+    
+}
+
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    if(amigo == ''){
+        alert('No se admite enviar un nombre vacío, ingresa nuevamente.');
+    } else {
+         listaDeAmigos.push(amigo);
+    }
+
+    console.log(listaDeAmigos);
+    
+    agregarEnLista(amigo);
+    limpiarTextBox();
+    
+}
